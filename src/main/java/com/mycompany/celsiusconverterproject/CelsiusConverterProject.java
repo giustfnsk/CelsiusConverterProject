9a0789de38f384f0212c19e1866c728c542431d2
Giulia <114960909+giustfnsk@users.noreply.github.com>
2023-03-11 03:56:15 -0300

    Create the Celsius Converter GUI with the application logic

---
+++ src/main/java/com/mycompany/celsiusconverterproject/CelsiusConverterProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.celsiusconverterproject;
+
+/**
+ *
+ * @author giuli
+ */
+public class CelsiusConverterProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
